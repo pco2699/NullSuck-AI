7e8a22f9189c5836809df0865adb339e27495871
pco2699 <aenkun@gmail.com>
2019-03-18 09:37:47 +0900

    .gitnore追加

--- server/api.py
+++ server/api.py
@@ -4,10 +4,11 @@ import os
 from os.path import join, dirname
 from dotenv import load_dotenv
 
-dotenv_path = join(dirname(__file__), '/config/env', '.env', '.' + os.environ.get('PY_ENV') or 'local')
+dotenv_path = join(dirname(__file__), '/config/env', '.env', '.' + os.getenv('PY_ENV', 'local'))
 load_dotenv(dotenv_path)
 
 api = responder.API()
+
 
 @api.route("/")
 def hello_world(req, resp):
